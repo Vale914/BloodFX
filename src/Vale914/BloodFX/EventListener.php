ff9f36888957690b287a29ff07a12c714a6c7906
Vale914 <valentinmaxim400@yahoo.it>
2020-01-05 20:41:39 +0100

    Initial commit

---
+++ src/Vale914/BloodFX/EventListener.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types=1);
+
+namespace Vale914\BloodFX;
+
+use pocketmine\block\Block;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\event\Listener;
+use pocketmine\level\particle\DestroyBlockParticle;
+use pocketmine\math\Vector3;
+
+class EventListener implements Listener{
+
+    /** @var Main $plugin */
+    private $plugin;
+
+    /**
+     * EventListener constructor.
+     * @param Main $plugin
+     */
+    public function __construct(Main $plugin){
+        $this->plugin = $plugin;
+    }
+
+    public function onDamage(EntityDamageEvent $event) : void{
+        if($event instanceof EntityDamageByEntityEvent){
+            $player = $event->getEntity();
+            $player->getLevel()->addParticle(new DestroyBlockParticle(new Vector3($player->x, $player->y + 1, $player->z), Block::get(Block::REDSTONE_BLOCK)));
+        }
+    }
+}
---
+++ src/Vale914/BloodFX/Main.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+namespace Vale914\BloodFX;
+
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase{
+
+    public function onEnable(){
+        $this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
+    }
+}
