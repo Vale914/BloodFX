4295ba68f43a744b9f88f04fec549a27ad96f2cd
Vale914 <valentinmaxim400@yahoo.it>
2020-01-05 20:48:57 +0100

    Initial commit

--- src/Vale914/BloodFX/Main.php
+++ src/Vale914/BloodFX/Main.php
@@ -10,4 +10,8 @@ class Main extends PluginBase{
     public function onEnable(){
         $this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
     }
+
+    public function onDisable(){
+        //test
+    }
 }
