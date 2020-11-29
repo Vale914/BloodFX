9af5b73d1fc852446fe29cb7a37f7e73c9ecb00f
Vale914 <valentinmaxim400@yahoo.it>
2020-11-29 01:50:06 +0100

    Update config.yml

--- src/Vale914/BloodFX/Main.php
+++ src/Vale914/BloodFX/Main.php
@@ -4,11 +4,17 @@ declare(strict_types=1);
 namespace Vale914\BloodFX;
 
 use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat;
 
 class Main extends PluginBase{
 
     public function onEnable(){
         $this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
         $this->saveDefaultConfig();
+        if($this->getConfig()->exists("version") !== true || $this->getConfig()->get("version") != $this->getDescription()->getVersion()){
+            $this->getLogger()->info(TextFormat::AQUA . "Updating configuration..");
+            unlink($this->getDataFolder() . "config.yml");
+            $this->saveDefaultConfig();
+        }
     }
 }
